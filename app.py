5ef1bf854d222c2f7997227df82c27716293c342
Leonardo Mijares <leonardomjrs@gmail.com>
2022-07-04 21:25:42 -0300

    appis andando

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'sistema22072'
 mysql.init_app(app)
 
+CARPETA = os.path.join('uploads')
+app.config['CARPETA']=CARPETA
+
 @app.route('/')
 def func():
     sql="SELECT * FROM `EMPLEADOS`;"
@@ -29,8 +32,49 @@ def func():
 def destroy(id):
     conn=mysql.connect()
     cursor=conn.cursor()
+    
+    cursor.execute("SELECT foto from empleados WHERE id = %s", id)   
+    fila = cursor.fetchall() 
+    os.remove(os.path.join(app.config['CARPETA'], fila[0][0])) 
+        
     cursor.execute("DELETE FROM empleados WHERE id=%s", id)
     conn.commit()
+    return redirect('/')
+    
+@app.route("/edit/<int:id>")
+def edit(id):
+    conn=mysql.connect() 
+    cursor=conn.cursor() 
+    cursor.execute("SELECT * FROM empleados WHERE id=%s", id) 
+    empleados = cursor.fetchall()
+    print(empleados)
+    conn.commit() 
+    return render_template('empleados/edit.html' , empleados = empleados)
+    
+@app.route("/update" , methods=['POST'])
+def update():
+    _id = request.form['txtId']
+    _nombre = request.form['txtNombre']
+    _correo = request.form['txtCorreo']
+    _foto = request.files['txtFoto']
+    sql= "UPDATE empleados SET nombre = %s , correo = %s WHERE id = %s;"
+    datos = (_nombre, _correo, _id)
+    conn=mysql.connect() 
+    cursor=conn.cursor() 
+    now = datetime.now()
+    tiempo = now.strftime("%Y%H%M%S")
+    
+    if _foto.filename != '':
+        nuevoNombreFoto= tiempo + _foto.filename
+        _foto.save("uploads/"+nuevoNombreFoto) 
+        
+        cursor.execute("SELECT foto from empleados WHERE id = %s", _id)   
+        fila = cursor.fetchall() 
+        os.remove(os.path.join(app.config['CARPETA'], fila[0][0])) 
+        cursor.execute("UPDATE empleados SET foto = %s WHERE id = %s", (nuevoNombreFoto, _id))
+    
+    cursor.execute(sql,datos)
+    conn.commit() 
     return redirect('/')
     
     
@@ -51,6 +95,8 @@ def storage():
     if _foto.filename != '':
         nuevoNombreFoto= tiempo + _foto.filename
         _foto.save("uploads/"+nuevoNombreFoto)
+    else:
+        nuevoNombreFoto=''
     
     sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
     
@@ -59,7 +105,7 @@ def storage():
     cursor=conn.cursor() #llevar la sentencia sql (line 17) y llevarla a la DB
     cursor.execute(sql,datos) #ejecutar la consulta line17
     conn.commit() #mandar la consulta
-    return render_template('empleados/index.html')
+    return redirect('/')
 
 
 
